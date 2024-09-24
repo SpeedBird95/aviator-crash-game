4c3d8baadefb7a1555cba6c44e0465a2f29e5011
Ameen <ameen.alqasim@gmail.com>
2024-09-24 16:44:49 +0400

    Back to original

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import { mockGraphicProps } from '@/components/Graphic/Graphic.mocks.ts';
-import Graphic from '@/components/Graphic/Graphic.tsx';
+import Graphic, { mockGraphicProps } from '@/components/Graphic/Graphic';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
